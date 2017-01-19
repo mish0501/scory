851a708f36431954ea5bf841c4c18ac278e201b0
Михаил Георгиев <misho_0501@abv.bg>
2017-01-19 22:35:51 +0200

    Changed the logo and favicon to match the name

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,20 +6,14 @@
     <meta content='text/html;charset=utf-8' http-equiv='content-type'>
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="57x57" href="/assets/images/favicons/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/assets/images/favicons/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/assets/images/favicons/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/assets/images/favicons/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/assets/images/favicons/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/assets/images/favicons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/assets/images/favicons/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/assets/images/favicons/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-touch-icon-180x180.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-194x194.png" sizes="194x194">
-    <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="/assets/images/favicons/android-chrome-192x192.png" sizes="192x192">
     <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/assets/images/favicons/manifest.json">
+    <link rel="mask-icon" href="/assets/images/favicons/safari-pinned-tab.svg" color="#51b212">
+    <link rel="shortcut icon" href="/assets/images/favicons/favicon.ico">
+    <meta name="msapplication-config" content="/assets/images/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#51b212">
     <!-- /Favicons -->
 
     <!-- / bootstrap [required] -->
@@ -48,8 +42,8 @@
       <header>
         <nav class='navbar navbar-default'>
           <a class='navbar-brand' href='/admin/home'>
-            <img height="21" class="logo" alt="Flatty" src="/admin/assets/images/logo.svg" />
-            <img height="21" class="logo-xs" alt="Flatty" src="/admin/assets/images/logo_xs.svg" />
+            <img height="30" class="logo" alt="Flatty" src="/admin/assets/images/logo.svg" />
+            <img height="30" class="logo-xs" alt="Flatty" src="/admin/assets/images/logo_xs.svg" />
           </a>
           <a class='toggle-nav btn pull-left' href='#'>
             <i class='icon-reorder'></i>
