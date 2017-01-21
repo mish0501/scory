3762fa77c4cffcce5927d6117cf8a1f447947268
Михаил Георгиев <misho_0501@abv.bg>
2017-01-21 17:13:54 +0200

    Minor changes to the favicons

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -15,11 +15,13 @@
     <!-- Favicons -->
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="/assets/images/favicons/android-chrome-192x192.png" sizes="192x192">
     <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-16x16.png" sizes="16x16">
     <link rel="manifest" href="/assets/images/favicons/manifest.json">
     <link rel="mask-icon" href="/assets/images/favicons/safari-pinned-tab.svg" color="#49bf67">
-    <meta name="msapplication-TileColor" content="#49bf67">
-    <meta name="msapplication-TileImage" content="/assets/images/favicons/mstile-144x144.png">
+    <link rel="shortcut icon" href="/assets/images/favicons/favicon.ico">
+    <meta name="msapplication-config" content="/assets/images/favicons/browserconfig.xml">
     <meta name="theme-color" content="#49bf67">
     <!-- /Favicons -->
 
