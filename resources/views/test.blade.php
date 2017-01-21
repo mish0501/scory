cc2640205151c10d3c85ddfddd60df2f14da3d67
Михаил Георгиев <misho_0501@abv.bg>
2017-01-21 16:58:30 +0200

    Corected the link to the favicons.

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -13,13 +13,13 @@
     <link href="/assets/css/app.css" rel="stylesheet">
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#49bf67">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/assets/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/assets/images/favicons/manifest.json">
+    <link rel="mask-icon" href="/assets/images/favicons/safari-pinned-tab.svg" color="#49bf67">
     <meta name="msapplication-TileColor" content="#49bf67">
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="msapplication-TileImage" content="/assets/images/favicons/mstile-144x144.png">
     <meta name="theme-color" content="#49bf67">
     <!-- /Favicons -->
 
