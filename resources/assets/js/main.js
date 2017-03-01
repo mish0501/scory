ce67bc69be6f06f7321c841bc38c2ffa19cf4c5b
Михаил Георгиев <misho_0501@abv.bg>
2017-03-01 16:26:11 +0200

    Renamed to MessagesWidged and desing tweaked.

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,10 +2,10 @@ require('./bootstrap');
 
 import router from './router'
 import store from './vuex/store'
-import MessegesWidget from './components/AdminPages/MessegesWidget.vue'
+import MessagesWidget from './components/AdminPages/MessagesWidget.vue'
 import Loading from './components/Loading.vue'
 
-Vue.component('messeges-widget', MessegesWidget)
+Vue.component('messages-widget', MessagesWidget)
 Vue.component('loading', Loading)
 
 var App = new Vue({
